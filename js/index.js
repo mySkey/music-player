abce641fc8c7fd1384d95a733eefeaaf186d4e18
mySkey <476168710@qq.com>
2019-03-31 12:38:08 +0800

    更改服务器地址

--- js/index.js
+++ js/index.js
@@ -1,136 +1,12 @@
 window.addEventListener('DOMContentLoaded',function(e){
   var music_api_url = 'http://audio.22family.com/'
-  var image_api_url = 'http://audio.22family.com/myplayer/mini/'
-	var info=[];
-	var timeArr=[];
-	var showLrcArr=[];
-	var currentLrc=0;
-	var num = 0;
-	var timer;
-	var lrcDom;
-  //本来这个该用后台来传，根据不同的type：最新 热门 榜首分类排序；只实现建议功能，以后做音乐播放器也是用vue，原生工作量太大了
-  var musicList = [
-              {
-                musicUrl: '起风了.mp3',
-                imgUrl: '0.jpg',
-                sing: '起风了',
-                singer: '买辣椒也用卷'
-              },
-              { 
-                musicUrl: '说散就散.mp3',
-                imgUrl: '1.jpg',
-                sing: '说散就散',
-                singer: '袁娅维'
-              },
-              { 
-                musicUrl: '一百万个可能.mp3',
-                imgUrl: '2.jpg',
-                sing: '一百万个可能',
-                singer: 'Christine Welch'
-              },
-              { 
-                musicUrl: '体面.mp3',
-                imgUrl: '3.jpg',
-                sing: '体面',
-                singer: '于文文'
-              },
-             {
-                musicUrl: '岑宁儿 - 追光者.mp3',
-                imgUrl:'4.jpg',
-                sing:'追光者',
-                singer:'芩宁儿'
-             },
-             {
-                musicUrl: '广东爱情故事.mp3',
-                imgUrl:'5.jpg',
-                sing:'广东爱情故事',
-                singer:'广东雨神'
-             },
-             {
-                musicUrl: '成都.mp3',
-                imgUrl:'6.jpg',
-                sing:'成都',
-                singer:'赵雷'
-             },
-             {
-                musicUrl: '校长 - 带你去旅行.mp3',
-                imgUrl:'7.jpg',
-                sing:'带你去旅行',
-                singer:'校长'
-             },
-             {
-                musicUrl: '陈一发儿 - 牵丝戏.mp3',
-                imgUrl:'8.jpg',
-                sing:'牵丝戏',
-                singer:'陈一发儿'
-             },
-             {
-                musicUrl: '任然 - 空空如也.mp3',
-                imgUrl:'9.jpg',
-                sing:'空空如也',
-                singer:'任然'
-             },
-             {
-                musicUrl: '周杰伦 - 告白气球.mp3',
-                imgUrl:'10.jpg',
-                sing:'告白气球',
-                singer:'周杰伦'
-             },
-             {
-                musicUrl: '大壮 - 我们不一样.mp3',
-                imgUrl:'11.jpg',
-                sing:'我们不一样',
-                singer:'大壮'
-             },
-             {
-                musicUrl: '夏磊 - 可能否.mp3',
-                imgUrl:'12.jpg',
-                sing:'可能否',
-                singer:'夏磊'
-             },
-             {
-                musicUrl: '等你下课.mp3',
-                imgUrl:'13.jpg',
-                sing:'等你下课',
-                singer:'周杰伦'
-             },
-             {
-                musicUrl: '任然 - 三生.mp3',
-                imgUrl:'14.jpg',
-                sing:'三生',
-                singer:'任然'
-             },
-             {
-                musicUrl: '远走高飞.mp3',
-                imgUrl:'15.jpg',
-                sing:'远走高飞',
-                singer:'金志文'
-             },
-             {
-                musicUrl: '李袁杰 - 离人愁.mp3',
-                imgUrl:'16.jpg',
-                sing:'离人愁',
-                singer:'李袁杰'
-             },
-             {
-                musicUrl: '李玉刚 - 刚好遇见你.mp3',
-                imgUrl:'17.jpg',
-                sing:'刚好遇见你',
-                singer:'李玉刚'
-             },
-             {
-                musicUrl: '演员.mp3',
-                imgUrl:'18.jpg',
-                sing:'演员',
-                singer:'薛之谦'
-             },
-             {
-                musicUrl: '易燃易爆炸.mp3',
-                imgUrl:'20.jpg',
-                sing:'易燃易爆炸',
-                singer:'化晨宇'
-             }
-  ]
+  var image_api_url = 'http://audio.22family.com/myplayer/mini/',
+      timeArr=[],
+      showLrcArr=[],
+      currentLrc=0,
+      num = 0,
+      timer,
+      lrcDom;
   document.oncontextmenu=function(e){
     //或者return false;
     e.preventDefault();
@@ -150,24 +26,23 @@ window.addEventListener('DOMContentLoaded',function(e){
 	}
 	var player = new Player();
 	player.init();
-	var containerDom = document.getElementById('container');
-	var nextDom = document.getElementsByClassName('next')[0];
-	var afterDom = document.getElementsByClassName('after')[0];
-	var pauseDom =document.getElementsByClassName('pause')[0];
-	var boxDom = document.getElementsByClassName('box')[0];
-	var sliderDom = document.getElementsByClassName('slider')[0];
-	var swiperDom =document.getElementsByClassName('swiper')[0];
-	var detialDom =document.getElementsByClassName('detial')[0];
-	var textDom =document.getElementsByClassName('text')[0];
-	var scrollDom =document.getElementsByClassName('content')[0];
-	var nameDom =document.getElementsByClassName('music-name')[0];
-	var singerDom =document.getElementsByClassName('singer')[0];
-	var listDom =document.getElementsByClassName('list')[0];
-	var musicListDom =document.getElementsByClassName('music-list')[0];
-  var scrollProDom = document.getElementsByClassName('scroll-pro')[0];
-  var lrcPlayDom =document.getElementsByClassName('lrc-play')[0];
-  var lrcTimeDom = document.getElementsByClassName('lrc-time')[0];
-	var mark = true;
+  var containerDom = document.getElementById('container'),
+      nextDom = document.getElementsByClassName('next')[0],
+      afterDom = document.getElementsByClassName('after')[0],
+      pauseDom =document.getElementsByClassName('pause')[0],
+      boxDom = document.getElementsByClassName('box')[0],
+      sliderDom = document.getElementsByClassName('slider')[0],
+      swiperDom =document.getElementsByClassName('swiper')[0],
+      textDom =document.getElementsByClassName('text')[0],
+      scrollDom =document.getElementsByClassName('content')[0],
+      nameDom =document.getElementsByClassName('music-name')[0],
+      singerDom =document.getElementsByClassName('singer')[0],
+      listDom =document.getElementsByClassName('list')[0],
+      musicListDom =document.getElementsByClassName('music-list')[0],
+      scrollProDom = document.getElementsByClassName('scroll-pro')[0],
+      lrcPlayDom =document.getElementsByClassName('lrc-play')[0],
+      lrcTimeDom = document.getElementsByClassName('lrc-time')[0],
+      mark = true;
 	
 	pauseDom.onclick = play;
   function play(){
@@ -260,7 +135,6 @@ window.addEventListener('DOMContentLoaded',function(e){
 				sliderDom.style.left = boxDomLeft +'px';
 				audioDom.currentTime = audioDom.duration*dianji;
 	},false);
-	// detialDom.innerHTML = musicList[0].text[0];
 	//数据渲染，
 	var length = musicList.length;
   //console.log(length)
